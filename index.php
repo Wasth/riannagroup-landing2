919e093b7f00a194efd557800caa11f8c949a339
Rick <riasta@yandex.ru>
2018-07-22 23:31:03 +0300

    header is ready

--- index.php
+++ index.php
@@ -5,18 +5,43 @@
     <title>Риана Групп - производство до 500 тонн
         металлоконструкций в месяц
     </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/bluelogo.png">
     <script src="js/main.js"></script>
 </head>
 <body>
 
 <!-- HEADER START -->
 <div class="wrapper">
-    <div class="container header">
-
+    <div class="header">
+        <div class="left-header">
+            <div class="inline-block"><img src="img/headerlogo.png" alt=""></div>
+        </div>
+        <div class="middle-header">
+            <div class="inline-block">
+                <img src="img/phoneblue.png" alt="">
+            </div>
+            <div class="inline-block">
+                <p>Есть вопросы? Звоните прямо сейчас!</p>
+                <h2>8 (800) 350-09-19</h2>
+            </div>
+        </div>
+        <div class="right-header">
+            <div class="inline-block">
+                <img src="img/phoneblue.png" alt="">
+            </div>
+            <div class="inline-block">
+                <p>Отправьте заявку на почту</p>
+                <h2><a href="mailto:sales@opora-rg.ru">sales@opora-rg.ru</a></h2>
+            </div>
+        </div>
     </div>
 </div>
 <!-- HEADER FINISH -->
+
+
 
 </body>
 </html>
