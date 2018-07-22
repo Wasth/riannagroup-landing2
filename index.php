3bcaaafd4bfb3113381b7bccb68732fb18381168
Rick <riasta@yandex.ru>
2018-07-22 22:39:09 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Риана Групп - производство до 500 тонн
+        металлоконструкций в месяц
+    </title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js"></script>
+</head>
+<body>
+
+<!-- HEADER START -->
+<div class="wrapper">
+    <div class="container header">
+
+    </div>
+</div>
+<!-- HEADER FINISH -->
+
+</body>
+</html>
